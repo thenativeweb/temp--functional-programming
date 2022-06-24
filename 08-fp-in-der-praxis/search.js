ff17a4cda42a30bf1fce1d5754d26ec6d8112f74
Golo Roden <golo.roden@thenativeweb.io>
2022-06-24 15:35:08 +0200

    Add more samples.

---
+++ 08-fp-in-der-praxis/search.js
@@ -0,0 +1,34 @@
+'use strict';
+
+function abs (x) {
+  return x >= 0 ? x : -x;
+}
+
+function search (validCommands, actualCommand) {
+  return validCommands.map(validCommand => {
+    const quality = 1 / abs(validCommand.length - actualCommand.length) * 0.999;
+
+    return {
+      value: validCommand,
+      quality: validCommand === actualCommand ?
+        1 :
+        quality === Number.POSITIVE_INFINITY ? 0.999 : quality
+    };
+  });
+}
+
+function getExactMatch (commandsResult) {
+  const exactMatches = commandsResult.
+    filter(commandResult => commandResult.quality === 1);
+
+  return exactMatches.length > 0 ? exactMatches[0] : undefined;
+}
+
+function getSuggestions (commandsResult) {
+  return commandsResult.
+    filter(commandResult => commandResult.quality < 1).
+    filter(commandResult => commandResult.quality > 0.5).
+    map(commandsResult => commandsResult.value);
+}
+
+module.exports = { search, getExactMatch, getSuggestions };
